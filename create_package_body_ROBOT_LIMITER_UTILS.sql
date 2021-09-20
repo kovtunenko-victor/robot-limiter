303b4e1d7bf19b13e58b5dbbe1adda1898934b18
Ковтуненко Виктор Евгеньевич <kovtunenko_ve@open.ru>
2021-09-20 11:39:51 +0300

    refactoring file names

--- create_package_body_ROBOT_LIMITER_UTILS.sql
+++ create_package_body_ROBOT_LIMITER_UTILS.sql
@@ -1,11 +1,3 @@
-CREATE OR REPLACE PACKAGE ROBOT_LIMITER_UTILS AS
-  FUNCTION GET_EXTERNAL_BALANCE_ROBOT_LIMITER (contract_number in string, seq in string DEFAULT '1') RETURN number;
-  PROCEDURE SET_EXTERNAL_BALANCE_ROBOT_LIMITER(external_balance number, contract_number in string, seq in string DEFAULT '1');
-  PROCEDURE SET_EXTERNAL_ACCOUNT_BALANCE_ROBOT_LIMITER(external_balance number, last_operation_date date, contract_number in string, seq in string DEFAULT '1');
-  FUNCTION GET_EXTERNAL_ACCOUNT_ROBOT_LIMITER (contract_number in string, account_type in string DEFAULT 'ORG_ACC', seq in string DEFAULT '1') RETURN string;
-END ROBOT_LIMITER_UTILS;
-
-
 CREATE OR REPLACE PACKAGE BODY ROBOT_LIMITER_UTILS is
 
 /* Функция для получения доступного "внешнего" сальдо
